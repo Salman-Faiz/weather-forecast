03161a8706bd1f3e999c57904c25b7b977e6bffd
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-11-28 13:10:47 +0600

    project setUp and design Ui

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Components/Header/Header";
+import WeatherBoard from "./Components/Weather/WeatherBoard";
+
+
+
+const App = () => {
+  return (
+    <div className="grid place-items-center h-screen">
+    <Header />
+    <main>
+      <section>
+        <WeatherBoard />
+      </section>
+    </main>
+    </div>
+  );
+};
+
+export default App;
