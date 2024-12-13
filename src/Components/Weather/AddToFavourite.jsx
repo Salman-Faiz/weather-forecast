7a6c6284702f664fad2cd1d2c5b437619a7ced2e
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-12-13 11:15:34 +0600

    add local storage favourite data and toggole fav modal

--- src/Components/Weather/AddToFavourite.jsx
+++ src/Components/Weather/AddToFavourite.jsx
@@ -1,14 +1,34 @@
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import heartSvg from '../../assets/heart.svg'
 import redHeartSvg from '../../assets/heart-red.svg'
-import { FavouriteContext } from '../../context'
+import { FavouriteContext, WeatherContext } from '../../context'
+
 const AddToFavourite = () => {
-  const { addToFavourites, removeFromFavourite, favourites } =
-    useContext(FavouriteContext)
-  const [isFavourite, setIsFavourite] = useState(false)
-  const handleFavourites = () => {
-    setIsFavourite(!isFavourite)
-  }
+  const { addToFavourites, removeFromFavourites, favourites } =
+        useContext(FavouriteContext);
+
+    const { weatherData } = useContext(WeatherContext);
+
+    const [isFavourite, toggleFavourite] = useState(false);
+
+    const { latitude, longitude, location } = weatherData;
+
+    useEffect(() => {
+        const found = favourites.find((fav) => fav.location === location);
+        toggleFavourite(found);
+    }, []);
+
+    function handleFavourites() {
+        const found = favourites.find((fav) => fav.location === location);
+
+        if (!found) {
+            addToFavourites(latitude, longitude, location);
+        } else {
+            removeFromFavourites(location);
+        }
+        toggleFavourite(!isFavourite);
+    }
+
   return (
     <div className='md:col-span-2'>
       <div className='flex items-center justify-end space-x-6'>
@@ -17,7 +37,7 @@ const AddToFavourite = () => {
           className='text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]'
         >
           <span>Add to Favourite</span>
-          <img src={isFavourite ? redHeartSvg:heartSvg} alt='' />
+          <img src={isFavourite ? redHeartSvg : heartSvg} alt='' />
         </button>
       </div>
     </div>
--- src/provider/FavouriteProvider.jsx
+++ src/provider/FavouriteProvider.jsx
@@ -4,11 +4,11 @@ const FavouriteProvider = ({ children }) => {
   const [favourites, setFavourites] = useLocalStorage('favourites', [])
 
   const addToFavourites = ({ latitude, longitude, location }) => {
-    setFavourites(...favourites, {
+    setFavourites([...favourites, {
       latitude: latitude,
       longitude: longitude,
       location: location,
-    })
+    }])
   }
   const removeFromFavourite = location => {
     const restFavourites = favourites.filter(fav => fav.location !== location)
